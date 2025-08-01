703a41c9aca04d171372a78d8d66ba76d475858e
Your Name <you@example.com>
2025-08-01 21:04:31 +0300

    fifth commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Клацніть на логотип Грози та фізики щоб знати більше
       </p>
     </>
   )
