3616486bb3ae293b14a1d09b5a5360921b360076
Your Name <you@example.com>
2025-08-01 21:01:31 +0300

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           Кліків {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Щоб щось змінити перейдіть в цю папку <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
